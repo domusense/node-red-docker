3dace6089b00e85b6af9feb97e41024b144b6fc3
domusense <37619939+domusense@users.noreply.github.com>
2018-06-02 07:45:33 +0300

    Update build-rpi.sh
    Created volume that will contain the user data in docker.

--- build-rpi.sh
+++ build-rpi.sh
@@ -7,10 +7,26 @@
 
 NODE_RED_VERSION=10.3.0
 
+# Build docker image
 docker build -f rpi/Dockerfile -t domu/nodered-rpi:0.18.7 .
 
 if [ $? -ne 0 ]; then
     echo "ERROR: Docker build failed for rpi image"
+    exit 1
+fi
+
+# User data volume 
+docker volume create --name node_red_user_data
+
+if [ $? -ne 0 ]; then
+    echo "Cannot create volume for user data."
+    exit 1
+fi
+
+docker run -d -p 1880:1880 -v node_red_user_data:/data --name nodered domu/nodered-rpi:0.18.7 
+
+if [ $? -ne 0 ]; then
+    echo "ERROR: Docker container failed to start for rpi build."
     exit 1
 fi
 
@@ -22,13 +38,6 @@ fi
 #fi
 
 #docker run -d domu/nodered-rpi:0.18.7
-
-docker run -d -p 1880:1880 -v node_red_user_data:/data --name nodered domu/nodered-rpi:0.18.7 
-
-if [ $? -ne 0 ]; then
-    echo "ERROR: Docker container failed to start for rpi build."
-    exit 1
-fi
 
 #docker push nodered/node-red-docker:rpi
 
